12dbebbf93cad74b445f7beac5a3e0fb7c69ae79
Vinicius Oliveira de Freitas <vinicius.vof@outlook.com>
2020-04-24 14:36:20 -0300

    Finalização da SeekBar

--- player.js
+++ player.js
@@ -1,16 +1,14 @@
 import audios from './data.js';
-import { path } from './utils.js';
+import { path, secondsToMinutes } from './utils.js';
 import elements from './playerElements.js';
 export default {
     audioData: audios,
     currentAudio: {},
     currentPlaying: 0,
     isPlaying: false,
-    start() {
+    start() {        
         elements.get.call(this);
-        elements.action.call(this);
         this.update();
-        this.audio.onended = () => this.next();
     },
     play() {
         this.isPlaying = true;
@@ -39,6 +37,7 @@ export default {
             this.restart();
         }
         this.update();
+        this.play();
     },
     setVolume(value) {
         this.audio.volume = value / 100;
@@ -46,13 +45,20 @@ export default {
     setSeek(value) {
         this.audio.currentTime = value;
     },
+    timeUpdate(){
+        this.currentDuration.innerText = secondsToMinutes(this.audio.currentTime);
+        this.seekbar.value = this.audio.currentTime;
+    },
     update() {
         this.currentAudio = this.audioData[this.currentPlaying];
         this.cover.style.background = `url('${path(this.currentAudio.cover)}') no-repeat center center / cover`;
         this.title.innerText = this.currentAudio.title;
         this.artist.innerText = this.currentAudio.artist;
         elements.createAudioElement.call(this, path(this.currentAudio.file));
-        this.seekbar.max = this.audio.duration;
+
+        this.audio.onloadeddata = () => {
+            elements.action.call(this);
+        }
     },
     restart() {
         this.currentPlaying = 0;
--- playerElements.js
+++ playerElements.js
@@ -1,4 +1,5 @@
-export default {
+import { secondsToMinutes } from './utils.js';
+ export default {
   get() {
     this.cover = document.querySelector('.card-image');
     this.title = document.querySelector('.card-content h5');
@@ -7,17 +8,23 @@ export default {
     this.vol = document.querySelector('#vol');
     this.volume = document.querySelector('#vol-control');
     this.seekbar = document.querySelector('#seekbar');
+    this.currentDuration = document.querySelector("#current-duration");
+    this.totalDuration = document.querySelector("#total-duration");
   },
 
   createAudioElement(audio) {
     this.audio = new Audio(audio);
   },
   action() {
+    this.audio.onended = () => this.next(); 
+    this.audio.ontimeupdate = () => this.timeUpdate();
     this.playPause.onclick = () => this.togglePlayPause();
     this.vol.onclick = () => this.toggleMute();
     this.volume.oninput = () => this.setVolume(this.volume.value);
     this.volume.onchange = () => this.setVolume(this.volume.value);
     this.seekbar.oninput = () => this.setSeek(this.seekbar.value);
     this.seekbar.onchange = () => this.setSeek(this.seekbar.value);
+    this.seekbar.max = this.audio.duration;
+    this.totalDuration.innerText = secondsToMinutes(this.audio.duration);
   }
 }
